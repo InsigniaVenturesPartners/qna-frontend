bf90194f407f815fed0b5bc771363b2304bd01a9
Hendrik Tekayadi <hendrik@insignia.vc>
2019-04-02 15:29:47 +0700

    Implement React More util

---
+++ src/util/react_more/index.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactHtmlParser from 'react-html-parser'
+
+class ReadMore extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      charLimit: props.charLimit
+    };
+    this.initialState = this.state;
+  }
+
+  getReadMoreContent() {
+    const { charLimit } = this.state;
+    const { children, readMoreText, readLessText } = this.props;
+    const a = ReactHtmlParser(children)
+
+    if (children.length > charLimit) {
+      return (<span className="short-text">
+        {ReactHtmlParser(children).slice(0, 2)}
+        <span
+          className="readMoreText"
+          style={{ color: '#007bff', cursor: 'pointer' }}
+          role="presentation"
+          onClick={this.showLongText.bind(this)}
+        >
+          {readMoreText}
+        </span>
+      </span>);
+    } else if (children.length < charLimit) {
+      return (<span className="short-text">
+        {ReactHtmlParser(children)}
+      </span>);
+    }
+    return (<span className="short-text">
+      {ReactHtmlParser(children)}
+      <span
+        className="readMoreText"
+        style={{ color: '#007bff', cursor: 'pointer' }}
+        role="presentation"
+        onClick={this.showShortText.bind(this)}
+      >
+        {readLessText}
+      </span>
+    </span>);
+  };
+
+  showLongText() {
+    const { children } = this.props;
+    this.setState({ charLimit: children.length });
+    this.getReadMoreContent();
+  }
+
+  showShortText() {
+    this.setState(this.initialState);
+    this.getReadMoreContent();
+  }
+
+  render() {
+    return (
+      <div>
+        {this.getReadMoreContent()}
+      </div>
+    );
+  }
+}
+
+ReadMore.propTypes = {
+  charLimit: PropTypes.number,
+  readMoreText: PropTypes.string,
+  readLessText: PropTypes.string,
+  children: PropTypes.string.isRequired
+};
+ReadMore.defaultProps = {
+  charLimit: 150,
+  readMoreText: 'Read more',
+  readLessText: 'Read less'
+};
+export default ReadMore;
